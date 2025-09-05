439b0b430ef9016d7a7c1ab0f455e85043a063c4
earthgouer <18811346065@163.com>
2025-09-05 16:42:59 +0800

    Update util.go

--- util.go
+++ util.go
@@ -1,6 +1,19 @@
-package utils
+package main
 
-// Add 返回两个整数之和
-func Add(a int, b int) int {
-    return a + b
+import (
+    "embed"
+    "log"
+)
+
+//go:embed internal/assets/data.zip
+var zipFile embed.FS // 声明一个嵌入的文件系统变量
+
+func main() {
+    // 现在你可以直接读取 zipFile 这个变量，它包含了 data.zip 的内容
+    data, err := zipFile.ReadFile("data.zip")
+    if err != nil {
+        log.Fatal(err)
+    }
+    // 然后用 archive/zip 包处理 data 字节切片...
+    log.Printf("ZIP file size: %d bytes\n", len(data))
 }
