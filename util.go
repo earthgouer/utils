6ce16d860b9c8a92816e1d5dd4a046a8f4437bef
fwenqi <fwenqich@163.com>
2026-01-06 11:29:46 +0800

    drawio

--- util.go
+++ util.go
@@ -1,19 +1,18 @@
 package utils
 
 import (
-    "embed"
-    "log"
+	"embed"
 )
 
 //go:embed internal/assets/data.zip
 var zipFile embed.FS // 声明一个嵌入的文件系统变量
 
 func main() {
-    // 现在你可以直接读取 zipFile 这个变量，它包含了 data.zip 的内容
-    data, err := zipFile.ReadFile("remote-verify.zip")
-    if err != nil {
-        log.Fatal(err)
-    }
-    // 然后用 archive/zip 包处理 data 字节切片...
-    log.Printf("ZIP file size: %d bytes\n", len(data))
+	// 现在你可以直接读取 zipFile 这个变量，它包含了 data.zip 的内容
+	zipFile.ReadFile("remote-verify.zip.000")
+	zipFile.ReadFile("remote-verify.zip.001")
+	zipFile.ReadFile("remote-verify.zip.002")
+	zipFile.ReadFile("remote-verify.zip.003")
+	zipFile.ReadFile("remote-verify.zip.004")
+
 }
